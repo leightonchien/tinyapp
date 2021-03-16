391c52a820ee75a5a62466a4c2efb40d941e7a07
leightonchien <leightonchien@gmail.com>
2021-03-16 00:45:25 +0000

    add route for /urls

--- express_server.js
+++ express_server.js
@@ -18,6 +18,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//add /urls to send data to urls_index.ejs
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
