69b4204de918c18e07b9ea660ffd25994d10b45b
leightonchien <leightonchien@gmail.com>
2021-03-16 01:24:22 +0000

    add short urls

--- express_server.js
+++ express_server.js
@@ -28,6 +28,14 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
