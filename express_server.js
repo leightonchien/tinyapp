99409e58219dd4883e678c3b0e6adc7b3fd841fa
leightonchien <leightonchien@gmail.com>
2021-03-18 07:19:20 +0000

    registration, error handling, login

--- express_server.js
+++ express_server.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const cookie = require('cookie-parser');
 const morgan = require('morgan');
 
+//Bring in Helper functions from separate file
+const {verifyShortUrl, randomString, checkIfAvail, addUser, fetchUserInfo} = require('./helperFunctions');
+
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookie());
@@ -19,18 +22,26 @@ const urlDatabase = {
 };
 
 //store user as key object
-const userDatabase = {
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
-  },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
+// const userDatabase = {
+//   "userRandomID": {
+//     id: "userRandomID", 
+//     email: "user@example.com", 
+//     password: "purple-monkey-dinosaur"
+//   },
+//  "user2RandomID": {
+//     id: "user2RandomID", 
+//     email: "user2@example.com", 
+//     password: "dishwasher-funk"
+//   }
+// }
+
+const currentUser = cookie => {
+  for (let ids in userDatabase) {
+    if (cookie === ids) {
+      return userDatabase[ids]['email-address'];
+    }
   }
-}
+};
 
 app.get("/", (req, res) => {
   res.send("Hello sunshine!");
@@ -46,22 +57,27 @@ app.get("/hello", (req, res) => {
 
 //add /urls to send data to urls_index.ejs, all urls displayed on main page
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, user_id: req.cookies['user_id'] };
+  let templateVars = { urls: urlDatabase, current_user: currentUser(req.cookies['user_id']) };
   res.render("urls_index", templateVars);
 });
 
 //new url is created
 app.get("/urls/new", (req, res) => { 
-  let templateVars = { user_id: req.cookies['user_id']}
+  const current_user = currentUser(req.cookies['user_id'])
+  if (!current_user) {
+    res.redirect('/login');
+  }
+
+  let templateVars = { current_user: current_user }
   res.render("urls_new", templateVars);
 });
 
 //tell browser to go to new page aka (redirect)
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  if (verifyShortUrl(shortURL)) {
+  if (verifyShortUrl(shortURL, urlDatabase)) {
     let longURL = urlDatabase[req.params.shortURL];
-    let templateVars = { shortURL: shortURL, longURL: longURL, user_id: req.cookies['user_id']
+    let templateVars = { shortURL: shortURL, longURL: longURL, current_user: currentUser(req.cookies['user_id'])
   };
     res.render("urls_show", templateVars);
   } else {
@@ -74,7 +90,7 @@ app.get("/urls/:shortURL", (req, res) => {
 //new url added to be shown with all urls
 app.post("/urls", (req, res) => {
 
-  const shortURL = generateShortURL();
+  const shortURL = randomString();
   const newURL = req.body.longURL;
   urlDatabase[shortURL] = newURL;
   res.redirect(`/urls/${shortURL}`);
@@ -84,7 +100,7 @@ app.post("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
 
   const shortURL = req.params.shortURL;
-  if (verifyShortUrl(shortURL)) {
+  if (verifyShortUrl(shortURL, urlDatabase)) {
     const longURL = urlDatabase[shortURL];
     res.redirect(longURL);
   } else {
@@ -108,114 +124,71 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect('/urls')
 });
   
-// endpoint to login
-app.post("/login", (req, res) => {
-  if (userDatabase[req.body.user_id]) {
-    const user_id = req.body.user_id;
-    res.cookie('user_id', user_id);
-  }
-  res.status(400).send('Oops! Something went wrong, please try again')
-});
 
-// endpoint to logout
-app.post("/logout", (req, res) => {
-  res.clearCookie('user_id');
-  res.redirect('/urls');
-});
 
 //User registration form added/page added to register
 app.get("/register", (req, res) => {
-  templateVars = { user_id:req.cookies['user_id']}
+  templateVars = { current_user: currentUser(req.cookies['user_id'])}
   res.render("urls_register", templateVars);
   res.redirect('/urls');
 })
 
-//add user if available
-const addUser = newUser => {
-  const newUserId = generateShortURL();
-  newUser.id = newUserId
-  userDatabase[newUserId] = newUser;
-  return newUser
-}
 
-//this is to check if emails are registered
-const checkIfAvail = (newVal, database) => {
-  for (user in database) {
-    if (!user[newVal]) {
-      return null;
-    }
-  }
-  return true;
-}
 
 app.post("/register", (req, res) => {
-  const {email, password} = req.body;
+  const {password} = req.body;
+  const email = req.body['email-address']
   if (email === '') {
     res.status(400).send('Email is required');
   } else if (password === '') {
     res.status(400).send('Password is required');
   } else if (!checkIfAvail(email, userDatabase)) {
-    res.status(400), send('This email is already registered')
-  }
-  newUser = addUser(req.body)
+    res.status(400).send('This email is already registered');
+  } else {
+  newUser = addUser(req.body, userDatabase)
   res.cookie('user_id', newUser.id)
+  }
+
+app.get("/login", (req, res) => {
+  templateVars = { current_user: currentUser(req.cookies['user_id']) }
+  res.render("login", templateVars);
+})
+
+app.post("/login", (req, res) => {
+const emailUsed = req.body['email-address'];
+const pwdUsed = req.body['password'];
+if (fetchUserInfo(emailUsed, userDatabase)) {
+  const password = fetchUserInfo(emailUsed, userDatabase).password;
+  const id = fetchUserInfo(emailUsed, userDatabase).id;
+  if (password !== pwdUsed) {
+    res.status(403).send('Error 403... re-enter your password')
+  } else {
+    res.cookie('user_id', id);
+    res.redirect('/urls');
+  }
+} else {
+  res.status(403).send('Error 403... email not found')
+}
+
+});
+
+// endpoint to logout
+app.post("/logout", (req, res) => {
+res.clearCookie('user_id');
+res.redirect('/urls');
+});
+
+
+
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
-
-
-
-//Helper functions////////////////////////////////////////////////////////////////////
-
-//Generate an "unique" shortURL, implement a function that returns a string of 6 random alphanumeric characters
-const generateRandomString = () => {
-  let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-};
-
-
-const generateShortURL = () => {
-  let randomString = '';
-  while (randomString.length < 6) {
-    randomString += generateRandomString();
-  }
-  return randomString;
-}; 
-
-//this func will show if short url exists
-const verifyShortUrl = URL => {
-  return urlDatabase[URL];
-}; 
+});
 
 
 
 
 
-
-////////////////////////////////////////////////////////////
-//example code removed from main code
-//a is not defined
-////////////////////////////////////////////////////////////
-//
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
-
-// app.post('/urls', (req, res) => {
-//   console.log(req.body);
-//   res.send('Ok');
-// });
