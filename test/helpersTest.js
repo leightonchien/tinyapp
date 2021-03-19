cada66c0f64614ca4f5169eee2b7d371feda7ae3
leightonchien <leightonchien@gmail.com>
2021-03-19 07:57:02 +0000

    all test passed

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,24 +1,40 @@
 const { assert } = require('chai');
+const { getUserByEmail } = require('../helperFunctions.js');
 
-const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
-  userRandomID: {
-    id: 'userRandomID',
-    email: 'user@example.com',
-    password: 'purple-monkey-dinosaur'
-  },
-  user2RandomID: {
-    id: 'user2RandomID',
-    email: 'user2@example.com',
-    password: 'dishwasher-funk'
-  }
-};
+    "userRandomID": {
+      id: "userRandomID",
+      email: "user@example.com",
+      password: "purple-monkey-dinosaur"
+    },
+    "user2RandomID": {
+      id: "user2RandomID",
+      email: "user2@example.com",
+      password: "dishwasher-funk"
+    }
+  };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail('user@example.com', users);
-    const expectedOutput = 'userRandomID';
-    // Write your assert statement here
+    const result = getUserByEmail('user@example.com', testUsers);
+    const expectedOutput = { id: "userRandomID", email: "user@example.com", password: "purple-monkey-dinosaur" };
+    assert.equal(result.id, expectedOutput.id);
+    assert.equal(result.email, expectedOutput.email);
+    assert.equal(result.password, expectedOutput.password);
   });
-});
+
+  it('should return a user with valid email', function() {
+    const result = getUserByEmail('user2@example.com', testUsers);
+    const expectedOutput = { id: "user2RandomID", email: "user2@example.com", password: "dishwasher-funk" };
+    assert.equal(result.id, expectedOutput.id);
+    assert.equal(result.email, expectedOutput.email);
+    assert.equal(result.password, expectedOutput.password);
+  });
+
+  it('should return undefined with an invalid email', function() {
+    const result = getUserByEmail('user@example', testUsers);
+    const expectedOutput = { id: "userRandomID", email: "user@example.com", password: "purple-monkey-dinosaur" };
+    assert.equal(result, undefined);
+  });
+}); 
